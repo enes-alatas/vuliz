cf1fa8853da7523b1241a3b87901efa575a87eaf
Enes Oğuzhan Alataş <oguzenesalatas@gmail.com>
2025-03-21 23:17:28 +0100

    Update FileProcessingError to remove unnecessary parameter

--- src/packages/errors.ts
+++ src/packages/errors.ts
@@ -2,7 +2,7 @@
  * Error thrown when there's a failure processing a file
  */
 export class FileProcessingError extends Error {
-  constructor(message: string, cause?: Error) {
+  constructor(message: string) {
     super(message);
     this.name = 'FileProcessingError';
   }
