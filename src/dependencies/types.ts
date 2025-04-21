e251dbf261c03ba0eaf940fc6758574f258868fe
Enes Oğuzhan Alataş <oguzenesalatas@gmail.com>
2025-04-21 18:24:05 +0200

    Add DependencyProvider as an interface for the classes extracting dependencies of a given package

--- src/dependencies/types.ts
+++ src/dependencies/types.ts
@@ -7,3 +7,16 @@ export interface Dependency {
   from: Package;
   to: Package;
 }
+
+/**
+ * Interface for providers that extract dependencies of the given package
+ */
+export interface DependencyProvider {
+  /**
+   * Extracts all dependencies for a given package
+   *
+   * @param pkg The package to analyze for dependencies
+   * @returns A promise that resolves to an array of dependencies
+   */
+  extractDependencies(pkg: Package): Promise<Dependency[]>;
+}
