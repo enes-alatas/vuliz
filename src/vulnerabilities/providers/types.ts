e0c54d761ac9df92afd74225ffd3346432155ee6
Enes Oğuzhan Alataş <oguzenesalatas@gmail.com>
2025-08-03 22:42:26 +0200

    Refactor VulnerabilityProvider to remove packageType member and check types per package

--- src/vulnerabilities/providers/types.ts
+++ src/vulnerabilities/providers/types.ts
@@ -5,6 +5,5 @@ import {PackageType} from 'src/packages/types';
  * Interface for vulnerability data providers
  */
 export interface VulnerabilityProvider {
-  packageType: PackageType;
   addVulnerabilities(packages: Package[]): Promise<Package[]>;
 }
