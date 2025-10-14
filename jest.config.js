55252a726ee4b924bb46b8f448233b71b574c0d3
Enes Oğuzhan Alataş <oguzenesalatas@gmail.com>
2025-10-15 00:12:11 +0200

    Update build and test configs for proper builds

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,5 @@ module.exports = {
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
   },
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
     ],
   },
   resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
     extensions: ['.ts', '.js'],
     fallback: {
       fs: false,
